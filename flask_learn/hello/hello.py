6fcdc72ed221e6307705d99ddee722ac6e55bd81
hj <408859584@qq.com>
2017-10-26 20:30:05 +0800

    modified:   hello.py

--- flask_learn/hello/hello.py
+++ flask_learn/hello/hello.py
@@ -3,14 +3,19 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
-from flask.ext.wtf import Form
-
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators = [Required()])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def hello_world():
