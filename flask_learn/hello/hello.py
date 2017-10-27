f2102e403150f21a92d314b2c657f97283d71fa8
hj <408859584@qq.com>
2017-10-27 16:22:21 +0800

    modified:   flask_learn/hello/hello.py 	modified:   flask_learn/hello/templates/base.html 	modified:   flask_learn/hello/templates/index.html

--- flask_learn/hello/hello.py
+++ flask_learn/hello/hello.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+#-*- coding:utf-8 -*-
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -14,12 +15,21 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 class NameForm(Form):
-    name = StringField('What is your name?', validators = [Required()])
-    submit = SubmitField('Submit')
+    name = StringField("What's your name?", validators = [Required()])
+    submit = SubmitField(u'提交')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you changed the name !')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('hello_world'))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
